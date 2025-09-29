3a1fb20003322411ff1d1c255e9f7be5fdd16a5a
ASHISH KUMAR JHA <ajha9108@gmail.com>
2025-09-29 16:37:38 +0530

    Create divisibleby7ornot.c

---
+++ divisibleby7ornot.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main() 
+    {
+    int a;
+    printf("Enter a number\n ");
+    scanf("%d", &a);
+        if (a%7==0)
+        {
+            printf(" number %d is divisible by 7\n", a);
+        }
+        else 
+        {
+            printf("%d is not divisible by 7", a);
+        }
+    return 0;
+    }
