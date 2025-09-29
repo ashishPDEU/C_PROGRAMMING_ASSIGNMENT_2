3749838901815628e5fe3f6fbf14f70523378e4f
ASHISH KUMAR JHA <ajha9108@gmail.com>
2025-09-29 17:06:16 +0530

    Create first_n_natural_numb_.c

---
+++ first_n_natural_numb_.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main() 
+    {
+        int n;
+        printf("enter any number:\t");
+        scanf("%d",&n);
+    int i = 1;
+    for(i=1;i<=n;i++)
+    {
+        printf("%d\t",i);
+    }
+return 0;
+    }
