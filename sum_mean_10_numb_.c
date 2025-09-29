3b30cb376d445d4a3f1aced1942e3869ee34167b
ASHISH KUMAR JHA <ajha9108@gmail.com>
2025-09-29 17:23:39 +0530

    Create sum_mean_10_numb_.c

---
+++ sum_mean_10_numb_.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int main() 
+    {
+        
+        float mean;
+       
+    int i = 1;
+    int sum=0;
+    int t;
+    for(i=1;i<=10;i++)
+    {
+        printf("enter number");
+        scanf("%d",&t);
+        sum+=t;
+        mean=sum/i;
+    }
+   
+     printf("sum total is %d\t",sum);
+     printf("average or mean is %f\t",mean);
+return 0;
+    }
