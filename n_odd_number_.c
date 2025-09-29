de519a56795de0857ea97e9ffac8b942ed913734
ASHISH KUMAR JHA <ajha9108@gmail.com>
2025-09-29 17:07:15 +0530

    Create n_odd_number_.c

---
+++ n_odd_number_.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main() 
+    {
+        int n;
+        printf("enter any number:\t");
+        scanf("%d",&n);
+    int i = 0;
+    for(i=0;i<n;i++)
+    {
+        printf("%d\t",2*i+1);
+    }
+return 0;
+    }
