c3c888918e24d05fbdc1bb28cd090ff0b7f82d6c
ASHISH KUMAR JHA <ajha9108@gmail.com>
2025-09-29 17:08:13 +0530

    Create n_even_number_.c

---
+++ n_even_number_.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main() 
+    {
+        int n;
+        printf("enter any number:\t");
+        scanf("%d",&n);
+    int i = 1;
+    for(i=1;i<=n;i++)
+    {
+        printf("%d\t",2*i);
+    }
+return 0;
+    }
