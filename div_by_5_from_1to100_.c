a4fac931ef120b5c6c7ff0d630a448470d60d4b5
ASHISH KUMAR JHA <ajha9108@gmail.com>
2025-09-30 12:34:12 +0530

    Create div_by_5_from_1to100_.c

---
+++ div_by_5_from_1to100_.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+   int n,i;
+   i=1;
+   for(i=0;i<=100;i++)
+   {
+       if(i%5==0)
+       {
+           printf("%d\n",i);
+       }
+   }
+
+    return 0;
+}
